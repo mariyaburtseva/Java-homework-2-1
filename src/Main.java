78a7bda80bfaadb2f9b17a8432674e5b94ce7843
Burtseva Mariya <mariyaburceva9898@mail.ru>
2020-12-12 20:06:08 +0300

    Added Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int translation = 500_000_000;
+        int total = balance + translation;
+        System.out.println(total);
+    }
+}
